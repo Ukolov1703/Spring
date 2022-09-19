2ed2bc73e125b31be61441556625c7ee2e1d855b
Ukolov1703 <80189833+Ukolov1703@users.noreply.github.com>
2022-09-19 21:28:33 +0700

    Update Surgeon.java

--- src/main/java/Lesson1DZ/Surgeon.java
+++ src/main/java/Lesson1DZ/Surgeon.java
@@ -2,7 +2,7 @@ package Lesson1DZ;
 
 public class Surgeon implements Doctor{
     @Override
-    public void cabinet() {
+    public void cabinet() { 
         System.out.println("cabinet number 300");
     }
 }
