12ae518637b7c54fc421ec6ffde744f8b6cd5cc1
Ukolov1703 <80189833+Ukolov1703@users.noreply.github.com>
2022-09-19 21:28:22 +0700

    Update Registry.java

--- src/main/java/Lesson1DZ/Registry.java
+++ src/main/java/Lesson1DZ/Registry.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class Registry {
     private Doctor doctor;
 
-    public Doctor getDoctor(){
+    public Doctor getDoctor(){ 
         return doctor;
     }
 
